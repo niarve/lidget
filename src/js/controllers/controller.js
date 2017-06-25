0af124de86c9879c3224515b36c426f445e2aa97
Nathan Iarve <nathan.iarve@gmail.com>
2017-06-25 00:39:02 -0400

    bootstrap works

--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -1,6 +1,4 @@
 "use strict";
 
 angular.module('app').controller('MainController', ['$scope', ($scope) => {
-  //write your controller here
-  $scope.greeting = { text: 'Hope you enjoy this basic NodeJs starter kit!'};
 }]);
