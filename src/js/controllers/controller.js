8d6d843b43560f2c989180389864671545ca6f12
Nathan Iarve <nathan.iarve@gmail.com>
2017-06-25 13:39:58 -0400

    learn page and routing fixed

--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -1,4 +1,14 @@
 "use strict";
 
 angular.module('app').controller('MainController', ['$scope', ($scope) => {
+  $scope.page = 'home';
+  console.log('showing', $scope.show);
+
+  $scope.route = function(page) {
+    $scope.page = page;
+  }
+
+  $scope.show = function(page) {
+    return $scope.page === page;
+  }
 }]);
